353f0dba7dca521403441144d69924a1c813401b
Chad Sehn <chad@baytek.ca>
2024-12-05 18:30:13 -0500

    Updating to add GitHub URIs to hopefully fix the WP auto updating

--- changelog.md
+++ changelog.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [1.1.2] - 2024-12-05
+### Changed
+- Updating to add GitHub URIs to hopefully fix the WP auto updating
+
 ## [1.1.1] - 2024-12-05
 ### Changed
 - Reordered changelog.md so updates are at the top of the file
--- main.php
+++ main.php
@@ -2,12 +2,15 @@
 
 /*
 Plugin Name: Baytek SendGrid Integration
+Plugin URI: https://github.com/baytek-team/sendgrid-integration
 Description: Use SendGrid for all system emails.
-Version: 1.1.0
+Version: 1.1.2
 Author: Baytek
 Author URI: https://www.baytek.ca
 Text Domain: baytek-sendgrid-integration
 Domain Path: /resources/assets/languages/
+GitHub Plugin URI: https://github.com/baytek-team/sendgrid-integration
+GitHub Branch: main
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
